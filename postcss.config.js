d689ede3b9f6e638ed5e38029caeb4febfa56a7b
Saneef Ansari <hello@saneef.com>
2024-04-15 18:45:54 +0530

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,48 @@
+import postcssProcessTokens from "postcss-design-token-utils";
+import postcssImport from "postcss-import";
+import postcssImportExtGlob from "postcss-import-ext-glob";
+import { clampGenerator, tokensToObject } from "./utils.js";
+
+import darkColorTokens from "./design-tokens/colors-dark.json" assert { type: "json" };
+import colorTokens from "./design-tokens/colors.json" assert { type: "json" };
+import fontTokens from "./design-tokens/font-family.json" assert { type: "json" };
+import spacingTokens from "./design-tokens/spacing.json" assert { type: "json" };
+import fontSizeTokens from "./design-tokens/text-sizes.json" assert { type: "json" };
+import fontWeightTokens from "./design-tokens/text-weights.json" assert { type: "json" };
+
+const color = tokensToObject(colorTokens.items);
+const darkColor = tokensToObject(darkColorTokens.items);
+const fontFamily = tokensToObject(fontTokens.items);
+const fontWeight = tokensToObject(fontWeightTokens.items);
+const spacing = tokensToObject(clampGenerator(spacingTokens.items));
+const fontSize = tokensToObject(clampGenerator(fontSizeTokens.items));
+
+const tokens = { color, darkColor, fontFamily, fontWeight, fontSize, spacing };
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const config = {
+	plugins: [
+		postcssImportExtGlob,
+		postcssImport,
+		postcssProcessTokens(tokens, {
+			customProperties: [
+				{
+					id: "darkColor",
+					prefix: "color",
+					group: "dark",
+				},
+			],
+			utilityClasses: [
+				{ id: "color", prefix: "text", property: "color" },
+				{ id: "color", prefix: "bg", property: "background-color" },
+				{ id: "fontFamily", prefix: "font", property: "font-family" },
+				{ id: "fontWeight", property: "font-weight" },
+				{ id: "fontSize", prefix: "text", property: "font-size" },
+				{ id: "spacing", prefix: "flow-space", property: "--flow-space" },
+			],
+		}),
+	],
+};
+
+export default config;
---
+++ utils.js
@@ -0,0 +1,59 @@
+import slugify from "slugify";
+
+/**
+ * Takes an array of tokens and sends back and array of name
+ * and clamp pairs for CSS fluid values.
+ *
+ * @param      {[{name: string, min: number, max: number}]}  tokens           The tokens
+ * @param      {object}  [viewports={}]        Viewports
+ * @param      {number}  [viewports.min=320]   Minimum viewport width
+ * @param      {number}  [viewports.max=1240]   Maximum viewport width
+ * @returns {Array<string|{name: string, value: string}>}
+ */
+export const clampGenerator = (tokens, { min = 320, max = 1240 } = {}) => {
+	const rootSize = 16;
+	const viewportsMin = min;
+	const viewportsMax = max;
+
+	return tokens.map(({ name, min, max }) => {
+		if (min === max) {
+			return `${min / rootSize}rem`;
+		}
+
+		// Convert the min and max sizes to rems
+		const minSize = min / rootSize;
+		const maxSize = max / rootSize;
+
+		// Convert the pixel viewport sizes into rems
+		const minViewport = viewportsMin / rootSize;
+		const maxViewport = viewportsMax / rootSize;
+
+		// Slope and intersection allow us to have a fluid value but also keep that sensible
+		const slope = (maxSize - minSize) / (maxViewport - minViewport);
+		const intersection = -1 * minViewport * slope + minSize;
+
+		return {
+			name,
+			value: `clamp(${minSize}rem, ${intersection.toFixed(2)}rem + ${(
+				slope * 100
+			).toFixed(2)}vw, ${maxSize}rem)`,
+		};
+	});
+};
+
+/**
+ * Converts human readable tokens into config friendly object
+ *
+ * @param {Array<{name: string, value: any}>} tokens
+ * @return {object} {key, value}
+ */
+export const tokensToObject = (tokens) => {
+	const nameSlug = (text) => slugify(text, { lower: true });
+	let response = {};
+
+	tokens.forEach(({ name, value }) => {
+		response[nameSlug(name)] = value;
+	});
+
+	return response;
+};
